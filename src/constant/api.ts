b2387025533e6931f58b352dcb328d9a71c2a65e
Manish Chaurasiya <chaurasiya.mns@gmail.com>
2024-07-07 17:28:38 +0530

    env changes made

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -2,7 +2,7 @@ const origin = window.location.origin;
 
 export const API_HOST = /localhost/.test(origin)
   ? "http://localhost:3004"
-  : `https://json-server-vercel-olive-iota.vercel.app`;
+  : "https://json-server-vercel-olive-iota.vercel.app"; // hosted url for json server
 
 export const API_URL = {
   TRANSACTIONS: "transactions",
